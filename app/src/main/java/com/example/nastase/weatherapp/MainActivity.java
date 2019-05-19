7d0d858d792304c0308d971646c6bfa6750eb51b
lv46cb <nastase.alexandru@hotmail.com>
2019-05-19 16:44:46 +0300

    Aproape final

--- app/src/main/java/com/example/nastase/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/nastase/weatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nastase.weatherapp;
 
 import android.Manifest;
+import android.app.NotificationChannel;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -9,10 +10,14 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +29,17 @@ import com.google.gson.reflect.TypeToken;
 import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import  android.app.NotificationManager;
+import  android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
@@ -36,6 +52,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     OpenWeatherMap openWeatherMap = new OpenWeatherMap();
 
     int MY_PERMISSION = 0;
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID=45612;
+
+    private boolean pref_notificari = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +92,68 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         Location location = locationManager.getLastKnownLocation(provider);
         if (location == null)
             Log.e("TAG", "No Location");
+
+
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+    }
+
+    public void clickPePoza(View view){
+
+        //Se ocupa de mesajul propriu-zis al notificarii
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker(txtCity.getText() + " - " + txtCelsius.getText());
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle(txtCity.getText());
+        notification.setContentText("Temperature : "+txtCelsius.getText()+" , " +txtDescription.getText());
+
+        //Se ocupa cu comportamentul in momentul in care dai click pe notificare
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Trimite notificarea
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID,notification.build());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.notifications_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.icon_notificari:
+
+                if(pref_notificari==false) {
+                    if(stopThread==false) {
+                        pref_notificari = true;
+                        Toast.makeText(this, "Notificatios enabled", Toast.LENGTH_SHORT).show();
+                        startThread();
+
+                        return true;
+                    }
+                }
+                else
+                {
+                    pref_notificari = false;
+                    Toast.makeText(this, "Notificatios disabled", Toast.LENGTH_SHORT).show();
+                    stopThread=true;
+
+                    return true;
+                }
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -176,4 +259,58 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                     .into(imageView);
         }
     }
+
+
+
+
+    private static final String TAG = "MainActivity";
+
+    private Handler mainHander = new Handler();
+
+    private volatile boolean stopThread = false;                 //volatile se asigura ca foloseste cea mai recenta versiune a variabilei, nu ceva cache-uit
+
+    private void startThread()
+    {
+        stopThread=false;
+        BackgroundRunnable runnable = new BackgroundRunnable(3600);
+        new Thread(runnable).start();
+    }
+
+    private void stopThread()
+    {
+        stopThread=true;
+    }
+
+    class BackgroundRunnable implements Runnable {
+
+        int seconds;
+
+        BackgroundRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for(int i=0;i<seconds;i++)
+            {
+                if(stopThread)
+                    return;
+                if(i==15)
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.performClick();
+                        }
+                    });
+
+                Log.d(TAG,"Start thread " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
